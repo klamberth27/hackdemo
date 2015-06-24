24d75ec1a778558f6199f6afbee48c2078a1678d
klamberth27 <kris@pilotport.com>
2015-06-24 13:40:50 -0500

    fixed responsive menu by putting require jquery at top of applications.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,8 +9,14 @@
 //
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
-//= require bootstrap
 //= require jquery
-//= require jquery_ujs
+
+
+//= require bootstrap
+
+
 //= require turbolinks
 //= require_tree .
+
+//= require jquery_ujs
+
